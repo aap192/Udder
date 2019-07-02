7f0e28a9c995bd41f93b018766f2654ce8c1b0c7
AkashPawar <akash.p4708@gmail.com>
2019-07-02 14:49:12 -0400

    Add label and images

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Akash Pawar on 7/2/19.
+//  Copyright © 2019 Akash Pawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
